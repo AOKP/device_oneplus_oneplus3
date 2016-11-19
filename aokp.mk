6025e650837553ff4d159ada5c069bf5a656fb26
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-19 07:56:10 -0800

    oneplus3: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    This partially reverts commit e20b77c.
    
    Change-Id: Iaabfaec2d7c734b5f286f3dc4ef651d93b2f2bc8

--- aokp.mk
+++ aokp.mk
@@ -19,9 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oneplus3 device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
-
 # Inherit some common AOKP stuff.
 $(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
